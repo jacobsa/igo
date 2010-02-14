bd2719df9ef649053aa796f440de4ee46bc06eef
Aaron Jacobs <aaronjjacobs@gmail.com>
2010-02-15 01:49:20 +1100

    Implemented deps.BuiltTotalOrder.

--- deps/deps.go
+++ deps/deps.go
@@ -6,12 +6,51 @@
 package deps
 
 import (
+	"container/vector"
 	"igo/set"
 )
 
 // BuildTotalOrder accepts a map from package names to the dependencies of
 // those packages, and returns a safe order in which to compile them (assuming
-// there are no circular dependencies).
-func BuildTotalOrder(dependencies map[string]*set.StringSet) []string {
-	return []string{}
+// there are no circular dependencies). The result will contain only those
+// packages which were present as keys in deps.
+func BuildTotalOrder(deps map[string]*set.StringSet) []string {
+	var visitor topologicalSortVisitor
+	visitor.nodes = make(map[string]*packageNode)
+	visitor.edges = deps
+
+	for key, _ := range deps {
+		visitor.nodes[key] = &packageNode{}
+	}
+
+	for key, _ := range deps {
+		visitor.Visit(key)
+	}
+
+	return visitor.result.Data()
+}
+
+type packageNode struct {
+	visited bool
+}
+
+// Implements a depth-first search topological sort algorithm for directed
+// acyclic graphs.
+type topologicalSortVisitor struct {
+	result vector.StringVector
+	nodes map[string]*packageNode
+	edges map[string]*set.StringSet
+}
+
+func (v *topologicalSortVisitor) Visit(name string) {
+  // Is this an unvisited node for a package we care about?
+	node, ok := v.nodes[name]
+	if !ok || node.visited { return }
+
+	node.visited = true
+	for otherName := range v.edges[name].Iter() {
+		v.Visit(otherName)
+	}
+
+	v.result.Push(name)
 }
--- deps/deps_test.go
+++ deps/deps_test.go
@@ -4,6 +4,7 @@
 package deps
 
 import (
+	"igo/set"
 	"reflect"
 	"testing"
 )
@@ -20,6 +21,21 @@ func indexOf(haystack []string, needle string) int {
 	return len(haystack)
 }
 
+func expectContains(t *testing.T, haystack []string, needle string) {
+	if indexOf(haystack, needle) == len(haystack) {
+		t.Errorf("Expected %v contains %s", haystack, needle)
+	}
+}
+
+func expectComesBefore(t *testing.T, array []string, a string, b string) {
+	expectContains(t, array, a)
+	expectContains(t, array, b)
+
+	if indexOf(array, a) > indexOf(array, b) {
+		t.Errorf("Expected %s comes before %s, got: %v", a, b, array)
+	}
+}
+
 func addDeps(depsMap map[string]*set.StringSet, name string, deps []string) {
 	var set set.StringSet
 	for _, val := range deps {
@@ -30,7 +46,7 @@ func addDeps(depsMap map[string]*set.StringSet, name string, deps []string) {
 }
 
 func TestEmptyMap(t *testing.T) {
-	input := make(map[string][]string)
+	input := make(map[string]*set.StringSet)
 	result := BuildTotalOrder(input)
 
 	if !reflect.DeepEqual(result, []string{}) {
@@ -39,7 +55,7 @@ func TestEmptyMap(t *testing.T) {
 }
 
 func TestSinglePackageNoDeps(t *testing.T) {
-	input := make(map[string][]string)
+	input := make(map[string]*set.StringSet)
 	addDeps(input, "foo", []string{})
 
 	expected := []string{"foo"}
@@ -51,7 +67,7 @@ func TestSinglePackageNoDeps(t *testing.T) {
 }
 
 func TestSinglePackageOnlyForeignDeps(t *testing.T) {
-	input := make(map[string][]string)
+	input := make(map[string]*set.StringSet)
 	addDeps(input, "foo", []string{"http", "os", "fmt"})
 
 	expected := []string{"foo"}
@@ -60,4 +76,62 @@ func TestSinglePackageOnlyForeignDeps(t *testing.T) {
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected: %v\nGot: %v", expected, result)
 	}
+}
+
+func TestTwoPackagesNoOrderingRestraints(t *testing.T) {
+	input := make(map[string]*set.StringSet)
+	addDeps(input, "foo", []string{"http", "os", "fmt"})
+	addDeps(input, "bar", []string{})
+
+	result := BuildTotalOrder(input)
+	if len(result) != 2 {
+		t.Errorf("Expected two elements, got %v", result)
+	}
+
+	expectContains(t, result, "foo")
+	expectContains(t, result, "bar")
+}
+
+func TestTwoPackagesWithOrderingRestraints(t *testing.T) {
+	input := make(map[string]*set.StringSet)
+	addDeps(input, "bar", []string{"http", "foo"})
+	addDeps(input, "foo", []string{})
+
+	result := BuildTotalOrder(input)
+	expected := []string{"foo", "bar"}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected: %v\nGot:%v", expected, result)
+	}
+}
+
+func TestFourPackages(t *testing.T) {
+	input := make(map[string]*set.StringSet)
+	addDeps(input, "foo", []string{"baz"})
+	addDeps(input, "bar", []string{"foo"})
+	addDeps(input, "baz", []string{})
+	addDeps(input, "tony", []string{"baz"})
+
+	result := BuildTotalOrder(input)
+	if len(result) != 4 {
+		t.Errorf("Expected four elements, got %v", result)
+	}
+
+	expectComesBefore(t, result, "baz", "foo")
+	expectComesBefore(t, result, "foo", "bar")
+	expectComesBefore(t, result, "baz", "tony")
+}
+
+func TestCircularDependency(t *testing.T) {
+	input := make(map[string]*set.StringSet)
+	addDeps(input, "foo", []string{"bar"})
+	addDeps(input, "bar", []string{"foo"})
+
+	// Should give some output.
+	result := BuildTotalOrder(input)
+	if len(result) != 2 {
+		t.Errorf("Expected two elements, got %v", result)
+	}
+
+	expectContains(t, result, "foo")
+	expectContains(t, result, "bar")
 }
