b0663a53545fc3ed0cba43dd7879c3f2a3bb05e4
Aaron Jacobs <aaronjjacobs@gmail.com>
2010-02-16 22:04:42 +1100

    Got main.go into a state where it can compile libraries.

--- main/main.go
+++ main/main.go
@@ -8,8 +8,73 @@ import (
 	"igo/deps"
 	"igo/set"
 	"os"
+	"path"
 	"strings"
 )
+
+// executeCommand runs the specified tool with the supplied arguments (not
+// including the path to the tool itself), chdir'ing to the specified directory
+// first. It returns true if and only if the child process returns zero.
+func executeCommand(tool string, args []string, dir string) bool {
+	fmt.Printf("%s %s\n", tool, strings.Join(args, " "))
+
+	var fullArgs vector.StringVector
+	fullArgs.Push(tool)
+	fullArgs.AppendVector(&args)
+
+	pid, err := os.ForkExec(
+		tool,
+		fullArgs.Data(),
+		os.Environ(),
+		dir,
+		[]*os.File{os.Stdin, os.Stdout, os.Stderr})
+	if err != nil {
+		panic(err)
+	}
+
+	waitMsg, err := os.Wait(pid, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	return waitMsg.ExitStatus() == 0
+}
+
+// compileFiles invokes 6g with the appropriate arguments for compiling the
+// supplied set of .go files, and exits the program if the subprocess fails.
+func compileFiles(files *set.StringSet, targetBaseName string) {
+	compilerPath := path.Join(os.Getenv("GOBIN"), "6g")
+	gopackPath := path.Join(os.Getenv("GOBIN"), "gopack")
+
+	targetDir, _ := path.Split(targetBaseName)
+	if targetDir != "" {
+		os.MkdirAll(path.Join("igo-out", targetDir), 0700)
+	}
+
+	// Compile
+	var compilerArgs vector.StringVector
+	compilerArgs.Push("-o")
+	compilerArgs.Push(targetBaseName + ".6")
+
+	for file := range files.Iter() {
+		compilerArgs.Push(path.Join("../", file))
+	}
+
+	if !executeCommand(compilerPath, compilerArgs.Data(), "igo-out/") {
+		os.Exit(1)
+	}
+
+
+	// Pack
+	var gopackArgs vector.StringVector
+	gopackArgs.Push("grc")
+	gopackArgs.Push(targetBaseName + ".a")
+	gopackArgs.Push(targetBaseName + ".6")
+
+	if !executeCommand(gopackPath, gopackArgs.Data(), "igo-out/") {
+		os.Exit(1)
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -25,10 +90,14 @@ func main() {
 	packageDeps := make(map[string]*set.StringSet)
 
 	var remainingDirs vector.StringVector
-	remainingDirs.Push(flag.Arg(1))
+	remainingDirs.Push("./" + flag.Arg(1))
 
 	for remainingDirs.Len() > 0 {
 		dir := remainingDirs.Pop()
+
+		// Have we already processed this directory?
+		_, alreadyDone := packageDeps[dir]
+		if alreadyDone { continue }
 
 		dirInfo := build.GetDirectoryInfo(dir, false)
 		if dirInfo.PackageName == "" {
@@ -36,17 +105,10 @@ func main() {
 			os.Exit(1)
 		}
 
-		// Have we already processed this package?
-		//
-		// TODO(jacobsa): It would be more efficient to do this chack before we hit
-		// the filesystem, based on directory name rather than package name.
-		_, alreadyDone := packageDeps[dirInfo.PackageName]
-		if alreadyDone { continue }
-
 		// Stash information about this package, and add its local dependencies to
 		// the queue.
-		requiredFiles[dirInfo.PackageName] = dirInfo.Files
-		packageDeps[dirInfo.PackageName] = dirInfo.Deps
+		requiredFiles[dir] = dirInfo.Files
+		packageDeps[dir] = dirInfo.Deps
 
 		for dep := range dirInfo.Deps.Iter() {
 			if strings.HasPrefix(dep, "./") { remainingDirs.Push(dep) }
@@ -67,5 +129,6 @@ func main() {
 	// Compile each of the packages in turn.
 	for _, currentPackage := range totalOrder {
 		fmt.Printf("\nCompiling package: %s\n", currentPackage)
+		compileFiles(requiredFiles[currentPackage], currentPackage)
 	}
 }
